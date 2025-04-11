66d84f712acbf688a7ccdb3df7157486e42cd70b
NicolasGarciaPosadas <ngarciaposadas@mail.austral.edu.ar>
2025-04-11 10:36:57 -0300

    hand in final

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,6 @@
 def leap_year():
     year = input("Ingrese un año: ")
     year = int(year)
-    if year%4 == 0 and year%400 ==0:
+    if year%4 == 0 or year%400 ==0:
         print(f"El año {year} es bisiesto")
     else:print(f"El año {year} no es bisiesto")
-
